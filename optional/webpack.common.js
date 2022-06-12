6cbc5bf0ae6a860e61afaed7ab2dac56d0069457
Marcel <marcelclavero94@gmail.com>
2022-06-12 12:56:33 +0200

    Optional bundling exercise

---
+++ optional/webpack.common.js
@@ -0,0 +1,41 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
+
+module.exports = {
+  context: path.join(__dirname, "./src"),
+  resolve: {
+    extensions: [".js", ".ts", ".tsx"],
+  },
+  entry: {
+    app: "./index.tsx",
+  },
+  output: {
+    filename: "[name].[chunkhash].js",
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        exclude: /node_modules/,
+        loader: "babel-loader",
+      },
+      {
+        test: /\.(png|jpg|gif)$/,
+        exclude: /node_modules/,
+        loader: "url-loader",
+      },
+      {
+        test: /\.html$/,
+        loader: "html-loader",
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: "index.html",
+      template: "./index.html",
+      scriptLoading: "blocking",
+    }),
+  ],
+};
